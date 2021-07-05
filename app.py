3fac8e7dab5502d2fb6ba3764b537d1e8b675652
abhi7927 <abhishekanavekar30@gmail.com>
2021-07-05 10:57:59 +0530

    second day

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 #url = "mailto:abhishekanavekar32@gmail.com?&subject=This is subject&body=This is body%0AThis is next line"
 
-def helper(to,subject,body):
-    url = "mailto:"+to+"?&subject="+subject+"&body="+body
-    print(url)
+def helper(fullname,to,subject,body):
+    url = "mailto:"+to+"?&subject="+subject+"&body="+body+fullname
+    #print(url)
     return url
 
 @app.route("/", methods=["POST","GET"])
@@ -15,13 +15,12 @@ def home():
     if request.method=='GET':
         return render_template("index.html")
     if request.method=='POST':
+        sender_name = request.form['fullname'] 
         to = request.form['to'] 
         subject = request.form['subject']
         body = request.form['body']
-        url = helper(to,subject,body)
+        url = helper(sender_name,to,subject,body)
         return redirect(url) 
-
-
 
 if __name__=="__main__":
     app.run(debug=True)
