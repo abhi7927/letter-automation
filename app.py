ffabff86c56b7b061a761c5444e0b59193db92ea
abhi7927 <abhishekanavekar30@gmail.com>
2021-07-06 18:48:50 +0530

    leave letter final

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 #url = "mailto:abhishekanavekar32@gmail.com?&subject=This is subject&body=This is body%0AThis is next line"
 
-def helper(fullname,to,subject,body):
-    string = "mailto:"+to+"?&subject="+subject+"&body="+body+fullname+"\n\n"
+def helper(to,subject,body):
+    string = "mailto:"+to+"?&subject="+subject+"&body="+body
     #print(url)
     url = convert_to_url_format(string)
     return url
@@ -20,11 +20,10 @@ def home():
     if request.method=='GET':
         return render_template("index.html")
     if request.method=='POST':
-        sender_name = request.form['fullname'] 
         to = request.form['to'] 
         subject = request.form['subject']
         body = request.form['body']
-        url = helper(sender_name,to,subject,body)
+        url = helper(to,subject,body)
         return redirect(url) 
 
 if __name__=="__main__":
--- static/fun.js
+++ static/fun.js
@@ -1,6 +1,14 @@
-function prefill() {
+function prefill() {    
+    var initialdate = document.querySelector("body > div > form > input[type=date]:nth-child(7)").value
+    var finaldate = document.querySelector("body > div > form > input[type=date]:nth-child(8)").value
+    var fullname = document.querySelector("body > div > form > input[type=text]:nth-child(1)").value
+    var duration = (Date.parse(finaldate)-Date.parse(initialdate))/(24*3600000)+1
     document.getElementById("body").value = "Respected Mr/Mrs,"+ "\n\n" +
-    "I would like to bring to your kind attention that due to (enter the reason),I would like to take a leave of absence for (number) days. I have handled my responsibility to (enter the names) and I am sure they will co-operate in my absence. I hope you understand my situation and grant me leave from (initial date) to (final date)."+
-    "\n\nThank You," + "\n\nYours Faithfully\n";
+    "I would like to bring to your kind attention that due to (enter the reason),"+
+    " I would like to take a leave of absence for " + duration +
+    " days. I have handled my responsibilities to (enter the names) and I am sure they will co-operate in my absence."+
+    " I hope you understand my situation and grant me leave from " +
+     initialdate +" to "+finaldate +"."+
+    "\n\nThank You," + "\n\nYours Faithfully\n"+fullname;
 }
   
