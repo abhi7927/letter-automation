294c90a075ed5d8ad8de1d005ec9b49960e7ddf9
abhi7927 <abhishekanavekar30@gmail.com>
2021-08-04 16:32:11 +0530

    new files

--- static/fun.js
+++ static/fun.js
@@ -1,6 +1,6 @@
-function prefill() {    
+function leaveLetterPrefill() {    
     var initialdate = document.querySelector("body > div > form > input[type=date]:nth-child(7)").value
-    var finaldate = document.querySelector("body > div > form > input[type=date]:nth-child(8)").value
+    var finaldate = document.querySelector("body > div > form > input[type=date]:nth-child(10)").value
     var fullname = document.querySelector("body > div > form > input[type=text]:nth-child(1)").value
     var duration = (Date.parse(finaldate)-Date.parse(initialdate))/(24*3600000)+1
     document.getElementById("body").value = "Respected Mr/Mrs,"+ "\n\n" +
@@ -11,4 +11,11 @@ function prefill() {
      initialdate +" to "+finaldate +"."+
     "\n\nThank You," + "\n\nYours Faithfully\n"+fullname;
 }
-  
+function requestDocsPrefill(){
+    var fullname = document.querySelector("body > div > form > input[type=text]:nth-child(1)").value
+    var documents = document.querySelector("body > div > form > input[type=text]:nth-child(7)").value
+    document.getElementById("body").value = "Respected Mr/Mrs,"+ "\n\n" +
+    "This letter is to inform you that for further process of your request we need the required documents as mentioned: "+
+    documents + "." + " You are requested to do the needful as early as possible." + 
+    "\n\nThank You," + "\n\n"+fullname;
+}  
